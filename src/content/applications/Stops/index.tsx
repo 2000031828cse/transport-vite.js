7bdf591e2dc13d3f850be439048b659735b99ba0
2000031828cse <121143584+2000031828cse@users.noreply.github.com>
2024-07-18 15:16:39 +0530

    Revert "Stops"
    This reverts commit 7a25ba3ded6fcafb7acb610c495db08d9a881d1c.

--- src/content/applications/Stops/index.tsx
+++ src/content/applications/Stops/index.tsx
@@ -1,290 +1,199 @@
-// // // import React from 'react';
-// // // import {
-// // //   Button,
-// // //   Container,
-// // //   Table,
-// // //   TableBody,
-// // //   TableCell,
-// // //   TableHead,
-// // //   TableRow,
-// // //   Box,
-// // //   Typography
-// // // } from '@mui/material';
-// // // import { useNavigate } from 'react-router-dom';
-// // // import { useStops } from './StopsContext';
+// import React from 'react';
+// import {
+//   Button,
+//   Container,
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableHead,
+//   TableRow,
+//   Box,
+//   Typography
+// } from '@mui/material';
+// import { useNavigate } from 'react-router-dom';
+// import { useStops } from './StopsContext';
 
-// // // const Stops: React.FC = () => {
-// // //   const { stops, deleteStop } = useStops();
-// // //   const navigate = useNavigate();
+// const Stops: React.FC = () => {
+//   const { stops, deleteStop } = useStops();
+//   const navigate = useNavigate();
 
-// // //   const handleAddStop = () => {
-// // //     navigate('/management/add-stop');
-// // //   };
+//   const handleAddStop = () => {
+//     navigate('/management/add-stop');
+//   };
 
-// // //   return (
-// // //     <Container
-// // //       maxWidth="lg"
-// // //       sx={{
-// // //         mt: 4,
-// // //         p: 3,
-// // //         border: '1px solid #ccc',
-// // //         borderRadius: '8px',
-// // //         backgroundColor: '#ffffff'
-// // //       }}
-// // //     >
-// // //       <Typography variant="h5" align="center" sx={{ mb: 3 }}>
-// // //         Manage Stops
-// // //       </Typography>
-// // //       <Box my={4}>
-// // //         <Button
-// // //           variant="contained"
-// // //           color="primary"
-// // //           onClick={handleAddStop}
-// // //           sx={{ mb: 3 }}
-// // //         >
-// // //           Add Stop
-// // //         </Button>
+//   return (
+//     <Container
+//       maxWidth="lg"
+//       sx={{
+//         mt: 4,
+//         p: 3,
+//         border: '1px solid #ccc',
+//         borderRadius: '8px',
+//         backgroundColor: '#ffffff'
+//       }}
+//     >
+//       <Typography variant="h5" align="center" sx={{ mb: 3 }}>
+//         Manage Stops
+//       </Typography>
+//       <Box my={4}>
+//         <Button
+//           variant="contained"
+//           color="primary"
+//           onClick={handleAddStop}
+//           sx={{ mb: 3 }}
+//         >
+//           Add Stop
+//         </Button>
 
-// // //         <Table>
-// // //           <TableHead>
-// // //             <TableRow>
-// // //               <TableCell>
-// // //                 <strong>Stop Number</strong>
-// // //               </TableCell>
-// // //               <TableCell>
-// // //                 <strong>Stop Name</strong>
-// // //               </TableCell>
-// // //               <TableCell>
-// // //                 <strong>Latitude</strong>
-// // //               </TableCell>
-// // //               <TableCell>
-// // //                 <strong>Longitude</strong>
-// // //               </TableCell>
-// // //               <TableCell>
-// // //                 <strong>Address</strong>
-// // //               </TableCell>
-// // //               <TableCell>
-// // //                 <strong>Landmark</strong>
-// // //               </TableCell>
-// // //               <TableCell>
-// // //                 <strong>Actions</strong>
-// // //               </TableCell>
-// // //             </TableRow>
-// // //           </TableHead>
-// // //           <TableBody>
-// // //             {stops.map((stop) => (
-// // //               <TableRow key={stop.number}>
-// // //                 <TableCell>{stop.number}</TableCell>
-// // //                 <TableCell>{stop.name}</TableCell>
-// // //                 <TableCell>{stop.latitude}</TableCell>
-// // //                 <TableCell>{stop.longitude}</TableCell>
-// // //                 <TableCell>{stop.address}</TableCell>
-// // //                 <TableCell>{stop.landmark}</TableCell>
-// // //                 <TableCell>
-// // //                   <Button
-// // //                     variant="contained"
-// // //                     color="secondary"
-// // //                     onClick={() => deleteStop(stop.number)}
-// // //                   >
-// // //                     Delete
-// // //                   </Button>
-// // //                 </TableCell>
-// // //               </TableRow>
-// // //             ))}
-// // //           </TableBody>
-// // //         </Table>
-// // //       </Box>
-// // //     </Container>
-// // //   );
-// // // };
+//         <Table>
+//           <TableHead>
+//             <TableRow>
+//               <TableCell>
+//                 <strong>Stop Number</strong>
+//               </TableCell>
+//               <TableCell>
+//                 <strong>Stop Name</strong>
+//               </TableCell>
+//               <TableCell>
+//                 <strong>Latitude</strong>
+//               </TableCell>
+//               <TableCell>
+//                 <strong>Longitude</strong>
+//               </TableCell>
+//               <TableCell>
+//                 <strong>Address</strong>
+//               </TableCell>
+//               <TableCell>
+//                 <strong>Landmark</strong>
+//               </TableCell>
+//               <TableCell>
+//                 <strong>Actions</strong>
+//               </TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {stops.map((stop) => (
+//               <TableRow key={stop.number}>
+//                 <TableCell>{stop.number}</TableCell>
+//                 <TableCell>{stop.name}</TableCell>
+//                 <TableCell>{stop.latitude}</TableCell>
+//                 <TableCell>{stop.longitude}</TableCell>
+//                 <TableCell>{stop.address}</TableCell>
+//                 <TableCell>{stop.landmark}</TableCell>
+//                 <TableCell>
+//                   <Button
+//                     variant="contained"
+//                     color="secondary"
+//                     onClick={() => deleteStop(stop.number)}
+//                   >
+//                     Delete
+//                   </Button>
+//                 </TableCell>
+//               </TableRow>
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </Box>
+//     </Container>
+//   );
+// };
 
-// // // export default Stops;
+// export default Stops;
 
-// // // import React from "react";
-// // // import {
-// // //   Button,
-// // //   Container,
-// // //   Table,
-// // //   TableBody,
-// // //   TableCell,
-// // //   TableHead,
-// // //   TableRow,
-// // //   Box,
-// // //   Typography,
-// // // } from "@mui/material";
-// // // import { useNavigate } from "react-router-dom";
-// // // import { useStops } from "./StopsContext";
 
-// // // const Stops: React.FC = () => {
-// // //   const { stops, deleteStop } = useStops();
-// // //   const navigate = useNavigate();
 
-// // //   const handleAddStop = () => {
-// // //     navigate("/management/add-stop");
-// // //   };
+// import React from "react";
+// import {
+//   Button,
+//   Container,
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableHead,
+//   TableRow,
+//   Box,
+//   Typography,
+// } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+// import { useStops } from "./StopsContext";
 
-// // //   return (
-// // //     <Container
-// // //       maxWidth="lg"
-// // //       sx={{
-// // //         mt: 4,
-// // //         p: 3,
-// // //         border: "1px solid #ccc",
-// // //         borderRadius: "8px",
-// // //         backgroundColor: "#ffffff",
-// // //         textAlign: "center", // Center aligns everything inside Container
-// // //       }}
-// // //     >
-// // //       <Typography variant="h5">Manage Stops</Typography>
-// // //       <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 3 }}>
-// // //         <Button variant="contained" color="primary" onClick={handleAddStop}>
-// // //           Add Stop
-// // //         </Button>
-// // //       </Box>
+// const Stops: React.FC = () => {
+//   const { stops, deleteStop } = useStops();
+//   const navigate = useNavigate();
 
-// // //       <Table>
-// // //         <TableHead>
-// // //           <TableRow>
-// // //             <TableCell>
-// // //               <strong>Stop Number</strong>
-// // //             </TableCell>
-// // //             <TableCell>
-// // //               <strong>Stop Name</strong>
-// // //             </TableCell>
-// // //             <TableCell>
-// // //               <strong>Latitude</strong>
-// // //             </TableCell>
-// // //             <TableCell>
-// // //               <strong>Longitude</strong>
-// // //             </TableCell>
-// // //             <TableCell>
-// // //               <strong>Landmark</strong>
-// // //             </TableCell>
-// // //             <TableCell>
-// // //               <strong>Actions</strong>
-// // //             </TableCell>
-// // //           </TableRow>
-// // //         </TableHead>
-// // //         <TableBody>
-// // //           {stops.map((stop) => (
-// // //             <TableRow key={stop.number}>
-// // //               <TableCell>{stop.number}</TableCell>
-// // //               <TableCell>{stop.name}</TableCell>
-// // //               <TableCell>{stop.latitude}</TableCell>
-// // //               <TableCell>{stop.longitude}</TableCell>
-// // //               <TableCell>{stop.landmark}</TableCell>
-// // //               <TableCell>
-// // //                 <Button
-// // //                   variant="contained"
-// // //                   color="secondary"
-// // //                   onClick={() => deleteStop(stop.number)}
-// // //                 >
-// // //                   Delete
-// // //                 </Button>
-// // //               </TableCell>
-// // //             </TableRow>
-// // //           ))}
-// // //         </TableBody>
-// // //       </Table>
-// // //     </Container>
-// // //   );
-// // // };
+//   const handleAddStop = () => {
+//     navigate("/management/add-stop");
+//   };
 
-// // // export default Stops;
+//   return (
+//     <Container
+//       maxWidth="lg"
+//       sx={{
+//         mt: 4,
+//         p: 3,
+//         border: "1px solid #ccc",
+//         borderRadius: "8px",
+//         backgroundColor: "#ffffff",
+//         textAlign: "center", // Center aligns everything inside Container
+//       }}
+//     >
+//       <Typography variant="h5">Manage Stops</Typography>
+//       <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 3 }}>
+//         <Button variant="contained" color="primary" onClick={handleAddStop}>
+//           Add Stop
+//         </Button>
+//       </Box>
 
-// // import React from "react";
-// // import {
-// //   Button,
-// //   Container,
-// //   Table,
-// //   TableBody,
-// //   TableCell,
-// //   TableHead,
-// //   TableRow,
-// //   Box,
-// //   Typography,
-// // } from "@mui/material";
-// // import { useNavigate } from "react-router-dom";
-// // import { useStops } from "./StopsContext";
+//       <Table>
+//         <TableHead>
+//           <TableRow>
+//             <TableCell>
+//               <strong>Stop Number</strong>
+//             </TableCell>
+//             <TableCell>
+//               <strong>Stop Name</strong>
+//             </TableCell>
+//             <TableCell>
+//               <strong>Latitude</strong>
+//             </TableCell>
+//             <TableCell>
+//               <strong>Longitude</strong>
+//             </TableCell>
+//             <TableCell>
+//               <strong>Landmark</strong>
+//             </TableCell>
+//             <TableCell>
+//               <strong>Actions</strong>
+//             </TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {stops.map((stop) => (
+//             <TableRow key={stop.number}>
+//               <TableCell>{stop.number}</TableCell>
+//               <TableCell>{stop.name}</TableCell>
+//               <TableCell>{stop.latitude}</TableCell>
+//               <TableCell>{stop.longitude}</TableCell>
+//               <TableCell>{stop.landmark}</TableCell>
+//               <TableCell>
+//                 <Button
+//                   variant="contained"
+//                   color="secondary"
+//                   onClick={() => deleteStop(stop.number)}
+//                 >
+//                   Delete
+//                 </Button>
+//               </TableCell>
+//             </TableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </Container>
+//   );
+// };
 
-// // const Stops: React.FC = () => {
-// //   const { stops, deleteStop } = useStops();
-// //   const navigate = useNavigate();
+// export default Stops;
 
-// //   const handleAddStop = () => {
-// //     navigate("/management/add-stop");
-// //   };
-
-// //   return (
-// //     <Container
-// //       maxWidth="lg"
-// //       sx={{
-// //         mt: 4,
-// //         p: 3,
-// //         border: "1px solid #ccc",
-// //         borderRadius: "8px",
-// //         backgroundColor: "#ffffff",
-// //         textAlign: "center",
-// //       }}
-// //     >
-// //       <Typography variant="h5">Manage Stops</Typography>
-// //       <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 3 }}>
-// //         <Button variant="contained" color="primary" onClick={handleAddStop}>
-// //           Add Stop
-// //         </Button>
-// //       </Box>
-
-// //       <Table>
-// //         <TableHead>
-// //           <TableRow>
-// //             <TableCell>
-// //               <strong>Stop Number</strong>
-// //             </TableCell>
-// //             <TableCell>
-// //               <strong>Stop Name</strong>
-// //             </TableCell>
-// //             <TableCell>
-// //               <strong>Latitude</strong>
-// //             </TableCell>
-// //             <TableCell>
-// //               <strong>Longitude</strong>
-// //             </TableCell>
-// //             <TableCell>
-// //               <strong>Landmark</strong>
-// //             </TableCell>
-// //             <TableCell>
-// //               <strong>Actions</strong>
-// //             </TableCell>
-// //           </TableRow>
-// //         </TableHead>
-// //         <TableBody>
-// //           {stops.map((stop) => (
-// //             <TableRow key={stop.id}>
-// //               <TableCell>{stop.id}</TableCell>
-// //               <TableCell>{stop.address}</TableCell>
-// //               <TableCell>{stop.lat}</TableCell>
-// //               <TableCell>{stop.lng}</TableCell>
-// //               <TableCell>{stop.landmark}</TableCell>
-// //               <TableCell>
-// //                 <Button
-// //                   variant="contained"
-// //                   color="secondary"
-// //                   onClick={() => deleteStop(stop.id)}
-// //                 >
-// //                   Delete
-// //                 </Button>
-// //               </TableCell>
-// //             </TableRow>
-// //           ))}
-// //         </TableBody>
-// //       </Table>
-// //     </Container>
-// //   );
-// // };
-
-// // export default Stops;
-
-// // 288
 
 import React from "react";
 import {
@@ -328,52 +237,50 @@ const Stops: React.FC = () => {
         </Button>
       </Box>
 
-      <Box sx={{ overflowX: "auto" }}>
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ minWidth: 100 }}>
-                <strong>Stop Number</strong>
-              </TableCell>
-              <TableCell sx={{ minWidth: 150 }}>
-                <strong>Stop Name</strong>
-              </TableCell>
-              <TableCell sx={{ minWidth: 100 }}>
-                <strong>Latitude</strong>
-              </TableCell>
-              <TableCell sx={{ minWidth: 100 }}>
-                <strong>Longitude</strong>
-              </TableCell>
-              <TableCell sx={{ minWidth: 150 }}>
-                <strong>Landmark</strong>
-              </TableCell>
-              <TableCell sx={{ minWidth: 100 }}>
-                <strong>Actions</strong>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <strong>Stop Number</strong>
+            </TableCell>
+            <TableCell>
+              <strong>Stop Name</strong>
+            </TableCell>
+            <TableCell>
+              <strong>Latitude</strong>
+            </TableCell>
+            <TableCell>
+              <strong>Longitude</strong>
+            </TableCell>
+            <TableCell>
+              <strong>Landmark</strong>
+            </TableCell>
+            <TableCell>
+              <strong>Actions</strong>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {stops.map((stop) => (
+            <TableRow key={stop.id}>
+              <TableCell>{stop.id}</TableCell>
+              <TableCell>{stop.address}</TableCell>
+              <TableCell>{stop.lat}</TableCell>
+              <TableCell>{stop.lng}</TableCell>
+              <TableCell>{stop.landmark}</TableCell>
+              <TableCell>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => deleteStop(stop.id)}
+                >
+                  Delete
+                </Button>
               </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {stops.map((stop) => (
-              <TableRow key={stop.id}>
-                <TableCell>{stop.id}</TableCell>
-                <TableCell>{stop.address}</TableCell>
-                <TableCell>{stop.lat}</TableCell>
-                <TableCell>{stop.lng}</TableCell>
-                <TableCell>{stop.landmark}</TableCell>
-                <TableCell>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => deleteStop(stop.id)}
-                  >
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Box>
+          ))}
+        </TableBody>
+      </Table>
     </Container>
   );
 };

a97ace09eb17437ad6adcc893dd6106b12670a3a
2000031828cse <121143584+2000031828cse@users.noreply.github.com>
2024-07-26 17:31:14 +0530

    pie chart in dashboard

--- src/content/dashboards/Admin/WatchList.tsx
+++ src/content/dashboards/Admin/WatchList.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { Box, Card } from '@mui/material';
-import WatchListColumn from './WatchListColumn';
+import React, { useState } from "react";
+import { Box, Card } from "@mui/material";
+import WatchListColumn from "./WatchListColumn";
 
 const WatchList = ({ stats }) => {
-  const [tabs, setTab] = useState<string | null>('watch_list_columns');
+  const [tabs, setTab] = useState<string | null>("watch_list_columns");
 
   return (
     <>
-      <Box display="flex" alignItems="center" justifyContent="space-between" sx={{ pb: 3 }}></Box>
-      {tabs === 'watch_list_columns' && <WatchListColumn stats={stats} />}
-      {!tabs && (
-        <Card sx={{ textAlign: 'center', p: 3 }}>
-          {/* <EmptyResultsWrapper src="/static/images/placeholders/illustrations/1.svg" /> */}
-        </Card>
-      )}
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      ></Box>
+      {tabs === "watch_list_columns" && <WatchListColumn stats={stats} />}
+      {!tabs && <Card sx={{ textAlign: "center" }}></Card>}
     </>
   );
 };
